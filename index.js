dfa8e0fc285c2852d497870bf46b068558206f3a
daniel-sanpedro <dpsanpedro@yahoo.com>
2024-06-18 16:15:21 -0700

    get added

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ require("dotenv/config");
 
 const api = process.env.API_URL;
 
-app.get(api + "/products", (req, res) => {
+app.get(`${api}/products`, (req, res) => {
+  const product = {
+    id: 1,
+    name: "hair dresser",
+    image: "some_url",
+  };
+  res.send(product);
+});
+
+app.post(api + "/products", (req, res) => {
   const product = {
     id: 1,
     name: "hair dresser",
