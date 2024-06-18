36019e0677c6c87aa9f4650ed8d28cdcaef6cfae
daniel-sanpedro <dpsanpedro@yahoo.com>
2024-06-18 16:12:34 -0700

    add

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.listen(3000, ()=> {
-    console.log('listening on port 3000');
+require("dotenv/config");
+
+const api = process.env.API_URL;
+
+app.get(api + "/products", (req, res) => {
+  const product = {
+    id: 1,
+    name: "hair dresser",
+    image: "some_url",
+  };
+  res.send(product);
+});
+
+app.listen(3000, () => {
+  console.log(api);
+  console.log("listening on port 3000");
 });
